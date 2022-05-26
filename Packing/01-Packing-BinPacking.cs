f5c71fb589386be009b631426a725d9aa99d1c4f
Jack Watts <jack.a.watts@gmail.com>
2022-05-26 23:16:59 +1000

    neaten samples

--- Packing/01-Packing-BinPacking.cs
+++ Packing/01-Packing-BinPacking.cs
@@ -1,7 +1,55 @@
-class DataModel
+using System;
+using Google.OrTools.LinearSolver;
+
+public class BinPackingMip
 {
-    public static double[] Weights = { 48, 30, 19, 36, 36, 27, 42, 42, 36, 24, 30 };
-    public int NumItems = Weights.Length;
-    public int NumBins = Weights.Length;
-    public double BinCapacity = 100.0;
+    class DataModel
+    {
+        public static double[] Weights = { 48, 30, 19, 36, 36, 27, 42, 42, 36, 24, 30 };
+        public int NumItems = Weights.Length;
+        public int NumBins = Weights.Length;
+        public double BinCapacity = 95.0;
+    }
+
+    public static void Main()
+    {
+        var data = new DataModel();
+        // TODO:
+
+
+
+
+
+
+        // SOLVE
+        var resultStatus = solver.Solve();
+
+        // Check that the problem has an optimal solution.
+        if (resultStatus != Solver.ResultStatus.OPTIMAL)
+        {
+            Console.WriteLine("The problem does not have an optimal solution!");
+            return;
+        }
+        Console.WriteLine($"Number of bins used: {solver.Objective().Value()}");
+        double TotalWeight = 0.0;
+        for (int j = 0; j < data.NumBins; ++j)
+        {
+            double BinWeight = 0.0;
+            if (binIsUsed[j].SolutionValue() == 1)
+            {
+                Console.WriteLine($"Bin {j}");
+                for (int i = 0; i < data.NumItems; ++i)
+                {
+                    if (itemBins[i, j].SolutionValue() == 1)
+                    {
+                        Console.WriteLine($"Item {i} weight: {DataModel.Weights[i]}");
+                        BinWeight += DataModel.Weights[i];
+                    }
+                }
+                Console.WriteLine($"Packed bin weight: {BinWeight}");
+                TotalWeight += BinWeight;
+            }
+        }
+        Console.WriteLine($"Total packed weight: {TotalWeight}");
+    }
 }
--- Scheduling/01-Scheduling.cs
+++ Scheduling/01-Scheduling.cs
@@ -585,6 +585,64 @@
                 }
             };
 
-            // TO BE CONTINUED...
+            // TODO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // SOLVE
+            var solver = new CpSolver();
+            CpSolverStatus status = solver.Solve(model);
+            Console.WriteLine($"Solve status: {status}");
+
+            if (status == CpSolverStatus.Optimal || status == CpSolverStatus.Feasible)
+            {
+                Console.WriteLine("Solution:");
+                foreach (var d in allDays)
+                {
+                    Console.WriteLine((DayOfWeek)(d));
+                    foreach (var e in allEmployees)
+                    {
+                        bool isWorking = false;
+                        foreach (int s in allShifts)
+                        {
+                            var key = Tuple.Create(e, d, s);
+                            if (solver.Value(shifts[key]) == 1L)
+                            {
+                                if (shiftRequests[e, d, s] == 1)
+                                {
+                                    Console.WriteLine($"  {employeeNames[e]} works shift {s} (requested).");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"  {employeeNames[e]} works shift {s} (not requested).");
+                                }
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(
+                    $"Number of shift requests met = {solver.ObjectiveValue} (out of {numEmployees * minShiftsPeremployee}).");
+            }
+            else
+            {
+                Console.WriteLine("No solution found.");
+            }
+
+            Console.WriteLine("Statistics");
+            Console.WriteLine($"  conflicts: {solver.NumConflicts()}");
+            Console.WriteLine($"  branches : {solver.NumBranches()}");
+            Console.WriteLine($"  wall time: {solver.WallTime()}s");
         }
     }
